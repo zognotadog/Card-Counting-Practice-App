d6c8e832a8e2eaf79a617f6a1d80e3da5670be4c
Alex Asher <alexasher@alexs-macbook-pro.local>
2017-07-18 00:14:02 +0100

    Completed app v1

--- Card Counter/ViewController.swift
+++ Card Counter/ViewController.swift
@@ -10,16 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cardView: UIImageView!
+    @IBOutlet weak var countLbl: UILabel!
+    @IBOutlet weak var showCountBtn: UIButton!
+    
+    var tap: UITapGestureRecognizer!
+    var count = 0
+    var countShown = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Setup card tap detection
+        tap = UITapGestureRecognizer(target: self, action: #selector(self.handleCardTap(_:)))
+        tap.isEnabled = true
+        cardView.addGestureRecognizer(tap)
+        
+    }
+    
+    @IBAction func resetBtnTap(_ sender: Any) {
+        //Reset
+        cardView.image = UIImage(named: "red_joker")
+        count = 0
+        if countShown {
+            countLbl.text = String(count)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func showCountBtnTap(_ sender: Any) {
+        if countLbl.text == "" {
+            countLbl.text = String(count)
+            showCountBtn.setTitle("Hide Count", for: .normal)
+            countShown = true
+        } else {
+            countLbl.text = ""
+            showCountBtn.setTitle("Show Count", for: .normal)
+            countShown = false
+        }
+        
     }
-
+    
+    func handleCardTap(_  sender: UITapGestureRecognizer){
+        print("Card tapped")
+        let card: Int = randomNum()
+        cardView.image = UIImage(named: "\(card)")
+        if (card < 6) || (13 < card && card < 19) || (26 < card && card < 32) || (39 < card && card < 45){
+            count += 1
+            print("Change: +1")
+        } else if (card > 5 && card < 9) || (card > 18 && card < 22) || (card > 31 && card < 35) || (card > 44 && card < 48) {
+            print ("Change: 0")
+        } else {
+            count -= 1
+            print("Change: -1")
+        }
+        
+        if countShown {
+            countLbl.text = String(count)
+        }
+        print("Count: \(count)")
+    }
+    
+    func randomNum() -> Int {
+        let cardNum  = (arc4random_uniform(51) + 1)
+        return Int(cardNum)
+    }
 
 }
 
